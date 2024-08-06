c1b6e835c496054cc645e401024816c3711d1e87
dean2699 <deanferrazzini@gmail.com>
2024-08-06 13:54:48 +0800

    All base functions are applied as well as Error Handling.

--- Code.js
+++ Code.js
@@ -10,28 +10,30 @@ function doPost(e) {
 
 function submitForm(formData) {
   var sheet = SpreadsheetApp.openById('11ECQ3ltFHMelIp03PhL4nS0qFywYkvcnGcalzvAMuEQ').getSheetByName("CLIENT PROFILE SHEET");
+  var response = "Client profile submitted successfully!";
   sheet.appendRow([
     new Date(),
     formData.name,
     formData.contact,
     formData.age,
-    formData.city,
+    formData.region,
     formData.gender,
     formData.clientType,
     formData.pwd
     // Add more fields as you expand the form
   ]);
-  return 'Form submitted successfully!';
+  return response;
 }
 
 function submitFeedback(feedbackData) {
   var sheet = SpreadsheetApp.openById('11ECQ3ltFHMelIp03PhL4nS0qFywYkvcnGcalzvAMuEQ').getSheetByName("CSM SHEET");
+  var response = "Client profile submitted successfully!";
   sheet.appendRow([
     new Date(), // Timestamp
     feedbackData.awareness,
     feedbackData.visibility,
     feedbackData.helpfulness,
-    feedbackData.overallGrade,
+    feedbackData.sqd0,
     feedbackData.sqd1,
     feedbackData.sqd2,
     feedbackData.sqd3,
@@ -42,4 +44,5 @@ function submitFeedback(feedbackData) {
     feedbackData.sqd8,
     feedbackData.comments
   ]);
+  return response;
 }
