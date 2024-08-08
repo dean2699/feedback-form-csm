d2622482a5aa4b542d8b42903a371fc613559cc3
dean2699 <deanferrazzini@gmail.com>
2024-08-08 11:03:06 +0800

    Final Commit placed censors for Data Privacy of the Office

--- Code.js
+++ Code.js
@@ -11,8 +11,8 @@ const lastrow_ba = burial_assistance_sheet.getLastRow();
 // Code.gs
 function doGet(e) {
   var output = HtmlService.createHtmlOutputFromFile('index');
-  output.setTitle("Southern Mindanao CSM");
-  output.setFaviconUrl('https://i.imgur.com/5B9zRCt.png');
+  output.setTitle("Client Feedback");
+  output.setFaviconUrl('[SET_IMAGE_LINK].png');
   return output;
 }
 
@@ -22,7 +22,7 @@ function doPost(e) {
 }
 
 function submitForm(formData) {
-  var sheet = SpreadsheetApp.openById('11ECQ3ltFHMelIp03PhL4nS0qFywYkvcnGcalzvAMuEQ').getSheetByName("CLIENT PROFILE SHEET");
+  var sheet = SpreadsheetApp.openById('[INPUT_GOOGLE_LINK]').getSheetByName("CLIENT PROFILE SHEET");
   var response = "Client profile submitted successfully!";
   sheet.appendRow([
     new Date(),
@@ -40,7 +40,7 @@ function submitForm(formData) {
 }
 
 function submitFeedback(feedbackData) {
-  var sheet = SpreadsheetApp.openById('11ECQ3ltFHMelIp03PhL4nS0qFywYkvcnGcalzvAMuEQ').getSheetByName("CSM SHEET");
+  var sheet = SpreadsheetApp.openById('[INPUT_GOOGLE_LINK]').getSheetByName("CSM SHEET");
   var response = "Client profile submitted successfully!";
   sheet.appendRow([
     new Date(), // Timestamp
@@ -75,10 +75,13 @@ function sortDataToSheets() {
   
   // Determine the appropriate sheet based on the trigger value
   var data_range;
+
   if (trigger === "Medical Assistance") {
     data_range = medical_assistance_sheet.getRange(lastrow_ma + 1, 1, 1, 20);
+
   } else if (trigger === "Burial Assistance") {
     data_range = burial_assistance_sheet.getRange(lastrow_ba + 1, 1, 1, 20);
+
   } else {
     Logger.log("Trigger not recognized: " + trigger);
     return; // Exit the function if trigger is not recognized
@@ -87,9 +90,8 @@ function sortDataToSheets() {
   // Define the data to be written into the selected sheet
   var data = [
     date_value, name_value, age_value, region_value, gender_value, type_value, pwd_value,
-    awareness_value, visibility_value, helpfulness_value,
-    sqd0_value, sqd1_value, sqd2_value, sqd3_value, sqd4_value, sqd5_value, sqd6_value, sqd7_value, sqd8_value,
-    comments_value
+    awareness_value, visibility_value, helpfulness_value,sqd0_value, sqd1_value, sqd2_value, 
+    sqd3_value, sqd4_value, sqd5_value, sqd6_value, sqd7_value, sqd8_value, comments_value
   ];
   
   // Write the data to the selected range
